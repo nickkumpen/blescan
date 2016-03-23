dd6bb36418861387bfb1559aeb662f7134f26c43
nickkumpen <nick.kumpen@hotmail.com>
2016-03-23 03:49:21 -0700

    add httplib2

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y python-dev python3-dev python3-setuptools python3-pip
 sudo pip-3.2 install pybluez
 sudo apt-get install -y python3-mysql.connector
 sudo apt-get install -y vim
-
+sudo apt-get install -y python3-httplib2
+sudo apt-get update && sudo apt-get upgrade
