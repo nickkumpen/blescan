384198d427cecd0c853870c38afd08c55093080d
nickkumpen <nick.kumpen@hotmail.com>
2016-03-23 03:22:47 -0700

    setup update

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #ble install
+sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install -y libbluetooth-dev bluez
 sudo apt-get install -y python-dev python3-dev python3-setuptools python3-pip
 sudo pip-3.2 install pybluez
 sudo apt-get install -y python3-mysql.connector
 sudo apt-get install -y vim
-sudo apt-get update && sudo apt-get upgrade
 
